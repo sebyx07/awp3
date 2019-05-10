ad9ed4e449592c9409fe7041ad1287fec0d6cffc
sebi <gore.sebyx@yahoo.com>
2019-05-10 14:53:32 +0300

    update

--- Gemfile
+++ Gemfile
@@ -3,4 +3,6 @@ source 'https://rubygems.org'
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 # Specify your gem's dependencies in awp3.gemspec
+
+gem 'rubocop'
 gemspec
--- lib/awp3/command.rb
+++ lib/awp3/command.rb
@@ -1,7 +1,18 @@
 module Awp3
   module Command
-    def exec_s(command)
+    def exec_s(command, show_output: false)
       p command
+
+      if show_output
+        print "\n"
+        result = `#{command}`
+        result.split("\n").each do |row|
+          next if row.blank? || row == "\n"
+
+          print "#{row.strip}\n"
+        end
+      end
+
       `#{command}`
     end
   end
